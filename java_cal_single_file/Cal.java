963acde53cd2f84057b626172d5c22b760ff1a37
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-07-21 13:20:14 +0530

    Add files via upload

---
+++ java_cal_single_file/Cal.java
@@ -0,0 +1,42 @@
+//import Scanner for get user inputs
+import java.util.Scanner;
+
+//create class for Cal
+public class Cal{
+    //create a method of get messages
+    /*
+    static void Msg(){
+        //create a aboject in Scanner 
+        Scanner Msgget = new Scanner(System.in);
+
+        System.out.println("Enter First Number : ");
+        int num = Msgget.nextInt();
+        System.out.println("First Name is " + fname);              
+    }*/
+
+    public void cal_numbers(int num1, int num2){
+        //num1 and num2 form user input numbers and the add all 2 numnbers
+        System.out.println("Total is : "+ (num1 + num2));
+    }
+
+
+
+    public static void main(String[] args) {
+        //now call the above created method
+        //Msg();
+
+        Cal calNum = new Cal();
+        Scanner GetNumbers = new Scanner(System.in);
+        
+        //get first number and assign to getNum1
+        System.out.println("Enter First Number : ");
+        int getNum1 = GetNumbers.nextInt();
+
+        //get Second Number and assign to getNum2
+        System.out.println("Enter Second Number : ");
+        int getNum2 = GetNumbers.nextInt();
+
+        //Pass above got numbers to add each other
+        calNum.cal_numbers(getNum1, getNum2);
+    }
+}
